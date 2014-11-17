fb83b0717526f9b5d9d732b2c8ead1f837bf7122
Antti Alhonen <antti.alhonen@tut.fi>
2014-11-17 07:34:36 +0200

    Fixes to laser cut code generation. It should be correct now.

--- cnc_gen.c
+++ cnc_gen.c
@@ -10,50 +10,79 @@
 #define CUT()   {if(ZMODE) fprintf(gfile, "G1 Z-5.0\n"); else fprintf(gfile, "M03\n");}
 #define UNCUT() {if(ZMODE) fprintf(gfile, "G1 Z5.0\n");  else fprintf(gfile, "M05\n");}
 
+// Generates main cell board, one side and one front.
+// Run the laser twice to obtain all 6 parts.
+
+// Saves weld data to gcode file as comments.
+// (WELDPOINT x_idx;y_idx;x_coord;y_coord) defines a center of the weld(s)
+// (ALIGNPOINT x_idx;y_idx;x_coord;y_coord) defines the coordinates of the corners
+// of the finished box. idx 0;0 is the bottom-left corner.
+// The box can be aligned against physical restrainers on two edges (typically bottom-left)
+
 int main(int argc, char** argv)
 {
 
-	float part_separation = 5.0;
+	float part_separation = 5.0; // clearance between main board, side and front
+	float thickness = 4.0; // Thickness of the material.
 	float cell_length = 65.0;
 	float hole = 18.2;
-	float wallgaps[4] = {5.0, 5.0, 5.0, 5.0};
+	float wallgaps[4] = {7.0, 1.8, 7.0, 1.8};
 	float cellgap = 1.8;
 	int ys[2];
 	int x;
-	float bonushole = 3.20;
+	float bonushole = 3.20; // Small extra holes in the main cell board. Manually
+				// make sure these are small enough not to overlap
+				// with cell holes. Can be used for wires or
+				// mounting. 3.20mm can be tapped to M4.
 
-	float lasertrim = 0.05;
+	float lasertrim = 0.05; // How much excess does the laser burn.
 
 
-	int do_sides = 1; // adds sides. Sides are solid.
-	float thickness = 5.0;
+	int do_sides = 1; // adds a side. Sides are solid, with small optional holes
+			  // for wires / mounting.
 	float finger_size_x = 8.0;
 	float finger_size_y = 10.0;
+
+	// Side bonusholes: small holes are inserted at every other cell location;
+	// two holes, one near top, one near bottom. Can be used for wires or
+	// mounting.
 	int do_side_bonusholes = 1;
-	float side_bonushole_size = 3.20;
-	float side_bonushole_dist = 5.0;
+	float side_bonushole_size = 3.20; // 3.20 tapped to M4 for mounting.
+	// Distance (of hole center) from the cell
+	// main board. Absolute minimum is side_bonushole_size/2.
+	float side_bonushole_dist = side_bonushole_size/2.0 + 5.0;
+
 	int do_fronts = 1; // adds front and back. Front and back 
-			   // look like //////, to allow air to pass with maximum
+			   // look like ####, to allow air to pass with maximum
 			   // structural integrity.
 
+	// How many fingers join the sides with fronts.
 	int num_side_front_fingers = 4;
 
+	// Solid parts between the finger part and ventilation holes.
 	float front_y_frame_width = 5.0;
+
+	// Width of the structural solid parts between the ventilation holes.
 	float front_mid_width = 4.0;
+	// A hole is generated at every cell location y[0] (the front cells
+	// from the ventilation hole viewpoint). num_front_holes_y specifies
+	// how many holes are generated per cell in cell height direction.
+	// Larger number adds more structural integrity.
 	int num_front_holes_y = 3;
 /*
 	FRONT & BACK
+                        v--- finger_size_x
            ###   ###   ###   ###   ###   ###   ###   ###   ###
 	#########################################################
-      #######################solid_width#########################
-      ####    #     #    #    #                                ####
-        ##    #     #    #    #                                ##
-        ##    #     #    #    #                                ##
-      #########     # w  #    #                                ####
-      ####    #     #    #    #    . . .                       ####
-        ###    #    #                                          ##
-        ##   #    #                                            ##
-      #### #step#                                              ####
+      #######################frame_width###########################
+      ####    #     #     #    #                               ####
+        ##    #     #     #    #                    ^          ##
+        ##    #     #     #    #                    |num_      ##
+      #########front_mid_width############# . . .   |front_    #### <-- finger_size_y
+      ####    #     #     #    #    . . .           |holes_y   ####
+       ###    #     #     #    #                    v          ##
+        ##    #     #     #         hole at every cell (y[0])  ##
+      ####    #     #            ...  <----------->            ####
       #############################################################
         #########################################################
            ###   ###   ###   ###   ###   ###   ###   ###   ###
@@ -111,6 +140,12 @@ int main(int argc, char** argv)
 		fprintf(gfile, " %s ", argv[i]);
 	}
 	fprintf(gfile, ")\n");
+	fprintf(gfile, "(%f; %f; %f; %f; %f; %f; %f; %f;)\n(%f; %f; %f; %f; %f; %f; %f; %d;)\n(%f; %f; %d)\n",
+		part_separation, thickness, cell_length, hole, wallgaps[0], wallgaps[1], wallgaps[2], wallgaps[3],
+		cellgap, bonushole, lasertrim, finger_size_x, finger_size_y, side_bonushole_size, side_bonushole_dist,
+		num_side_front_fingers,
+		front_y_frame_width, front_mid_width, num_front_holes_y);
+
 	fprintf(gfile, "G96 S1\n");
 	fprintf(gfile, "G21\n");
 	fprintf(gfile, "G61\n");
@@ -126,13 +161,13 @@ int main(int argc, char** argv)
 
 	if(do_fronts)
 	{
-		origin_x += cell_length + 3.0*thickness + part_separation;
+		origin_x += cell_length + part_separation;
 		side_origin_x = origin_x;
 	}
 
 	if(do_sides)
 	{
-		origin_y += cell_length + 3.0*thickness + part_separation;
+		origin_y += cell_length + part_separation;
 		front_origin_y = origin_y;
 	}
 
@@ -195,7 +230,8 @@ int main(int argc, char** argv)
 	outline[3][X] = origin_x;
 	outline[3][Y] = outline[2][Y];
 
-	fprintf(gfile, "G00 X%.2f Y%.2f (ALIGNPOINT 0;0;%.2f;%.2f)\n", outline[0][X]-lasertrim, outline[0][Y]-lasertrim, outline[0][X]-origin_x, outline[0][Y]-origin_y);
+	fprintf(gfile, "G00 X%.2f Y%.2f (ALIGNPOINT 0;0;%.2f;%.2f)\n", outline[0][X]-lasertrim, outline[0][Y]-lasertrim,
+		outline[0][X]-(do_fronts?thickness:0.0), outline[0][Y]-(do_sides?thickness:0.0));
 	CUT();
 
 
@@ -215,7 +251,8 @@ int main(int argc, char** argv)
 		}
 	}
 
-	fprintf(gfile, "G01 X%.2f Y%.2f (ALIGNPOINT 1;0;%.2f;%.2f)\n", outline[1][X]+lasertrim, outline[1][Y]-lasertrim, outline[1][X]-origin_x, outline[1][Y]-origin_y);
+	fprintf(gfile, "G01 X%.2f Y%.2f (ALIGNPOINT 1;0;%.2f;%.2f)\n", outline[1][X]+lasertrim, outline[1][Y]-lasertrim,
+		outline[1][X]+(do_fronts?thickness:0.0), outline[1][Y]-(do_sides?thickness:0.0));
 
 	// vertical right side
 	if(do_fronts)
@@ -232,7 +269,8 @@ int main(int argc, char** argv)
 		}
 	}
 
-	fprintf(gfile, "G01 X%.2f Y%.2f (ALIGNPOINT 1;1;%.2f;%.2f)\n", outline[2][X]+lasertrim, outline[2][Y]+lasertrim, outline[2][X]-origin_x, outline[2][Y]-origin_y);
+	fprintf(gfile, "G01 X%.2f Y%.2f (ALIGNPOINT 1;1;%.2f;%.2f)\n", outline[2][X]+lasertrim, outline[2][Y]+lasertrim,
+		outline[2][X]+(do_fronts?thickness:0.0), outline[2][Y]+(do_sides?thickness:0.0));
 
 	// horizontal top side
 
@@ -251,7 +289,8 @@ int main(int argc, char** argv)
 		}
 	}
 
-	fprintf(gfile, "G01 X%.2f Y%.2f (ALIGNPOINT 0;1;%.2f;%.2f)\n", outline[3][X]-lasertrim, outline[3][Y]+lasertrim, outline[3][X]-origin_x, outline[3][Y]-origin_y);
+	fprintf(gfile, "G01 X%.2f Y%.2f (ALIGNPOINT 0;1;%.2f;%.2f)\n", outline[3][X]-lasertrim, outline[3][Y]+lasertrim,
+		outline[3][X]-(do_fronts?thickness:0.0), outline[3][Y]+(do_sides?thickness:0.0));
 
 	// Vertical left side
 
@@ -278,8 +317,20 @@ int main(int argc, char** argv)
 	printf("Main panel size without fingers: %.2f x %.2f\n", outline[1][X]-outline[0][X], outline[2][Y]-outline[1][Y]);
 
 
+/*
 
-	float side_front_finger_step = (cell_length+2.0*thickness) / ((float)num_side_front_fingers);
+|||| thickness
+c ||||  < 0.5 steps
+e ||    ^ 1
+l ||||  v step
+l ||    ^ 1
+l ||||  v step
+e ||    ^ 1
+n ||||  v step
+|||| thickness
+
+*/
+	float side_front_finger_step = (cell_length - 2.0*thickness) / ((float)num_side_front_fingers-0.5);
 
 	// Do side panel
 	if(do_sides)
@@ -303,8 +354,8 @@ int main(int argc, char** argv)
 		// Right vertical
 		for(int cury = 0; cury < num_side_front_fingers; cury++)
 		{
-			float finger_start_y = side_origin_y + side_front_finger_step/2.0 +
-				side_front_finger_step*cury - side_front_finger_step/4.0;
+			float finger_start_y = side_origin_y + thickness +
+				side_front_finger_step*cury;
 			float finger_end_y = finger_start_y + side_front_finger_step/2.0;
 			fprintf(gfile, "G01 X%.2f Y%.2f\n", outline[1][X]+lasertrim, finger_start_y-lasertrim);
 			fprintf(gfile, "G01 X%.2f Y%.2f\n", outline[1][X]+thickness+lasertrim, finger_start_y-lasertrim);
@@ -312,7 +363,7 @@ int main(int argc, char** argv)
 			fprintf(gfile, "G01 X%.2f Y%.2f\n", outline[1][X]+lasertrim, finger_end_y+lasertrim);
 		}
 
-		fprintf(gfile, "G01 X%.2f Y%.2f\n", outline[1][X]+lasertrim, side_origin_y+cell_length+2.0*thickness+lasertrim);
+		fprintf(gfile, "G01 X%.2f Y%.2f\n", outline[1][X]+lasertrim, side_origin_y+cell_length+lasertrim);
 
 		// Top horizontal
 		for(int curx = x-1; curx >= 0; curx--)
@@ -320,20 +371,20 @@ int main(int argc, char** argv)
 			float finger_end_x = origin_x + wallgaps[0] + x_step*curx + hole/2.0 - finger_size_x/2.0;
 			float finger_start_x = finger_end_x + finger_size_x;
 
-			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_start_x-lasertrim, side_origin_y+cell_length+2.0*thickness+lasertrim);
-			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_start_x-lasertrim, side_origin_y+cell_length+thickness+lasertrim);
-			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_end_x+lasertrim, side_origin_y+cell_length+thickness+lasertrim);
-			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_end_x+lasertrim, side_origin_y+cell_length+2.0*thickness+lasertrim);
+			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_start_x-lasertrim, side_origin_y+cell_length+lasertrim);
+			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_start_x-lasertrim, side_origin_y+cell_length-thickness+lasertrim);
+			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_end_x+lasertrim, side_origin_y+cell_length-thickness+lasertrim);
+			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_end_x+lasertrim, side_origin_y+cell_length+lasertrim);
 		}
 
-		fprintf(gfile, "G01 X%.2f Y%.2f\n", outline[0][X]-lasertrim, side_origin_y+cell_length+2.0*thickness+lasertrim);
+		fprintf(gfile, "G01 X%.2f Y%.2f\n", outline[0][X]-lasertrim, side_origin_y+cell_length+lasertrim);
 
 		// Left vertical
 
 		for(int cury = num_side_front_fingers-1; cury >= 0; cury--)
 		{
-			float finger_end_y = side_origin_y + side_front_finger_step/2.0 +
-				side_front_finger_step*cury - side_front_finger_step/4.0;
+			float finger_end_y = side_origin_y + thickness +
+				side_front_finger_step*cury;
 			float finger_start_y = finger_end_y + side_front_finger_step/2.0;
 			fprintf(gfile, "G01 X%.2f Y%.2f\n", outline[0][X]-lasertrim, finger_start_y+lasertrim);
 			fprintf(gfile, "G01 X%.2f Y%.2f\n", outline[0][X]-thickness-lasertrim, finger_start_y+lasertrim);
@@ -357,7 +408,7 @@ int main(int argc, char** argv)
 					{
 						float bonushole_midx = origin_x + wallgaps[0] + x_step*curx + hole/2.0;
 						float bonushole_midy = cury?
-							(side_origin_y+cell_length+thickness-side_bonushole_dist):
+							(side_origin_y+cell_length-thickness-side_bonushole_dist):
 							(side_origin_y+thickness+side_bonushole_dist);
 						float bonushole_startx = bonushole_midx - side_bonushole_size/2.0;
 						float bonushole_startx_trimmed = bonushole_startx + lasertrim;
@@ -400,8 +451,8 @@ int main(int argc, char** argv)
 
 		for(int curx = 0; curx < num_side_front_fingers; curx++)
 		{
-			float finger_start_x = front_origin_x + side_front_finger_step/2.0 +
-				side_front_finger_step*curx - side_front_finger_step/4.0;
+			float finger_start_x = front_origin_x + thickness +
+				side_front_finger_step*curx;
 			float finger_end_x = finger_start_x + side_front_finger_step/2.0;
 			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_start_x+lasertrim, outline[0][Y]-thickness-lasertrim);
 			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_start_x+lasertrim, outline[0][Y]-lasertrim);
@@ -409,7 +460,7 @@ int main(int argc, char** argv)
 			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_end_x-lasertrim, outline[0][Y]-thickness-lasertrim);
 		}
 
-		fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length+2.0*thickness+lasertrim, outline[0][Y]-thickness-lasertrim);
+		fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length+lasertrim, outline[0][Y]-thickness-lasertrim);
 
 		// Right vertical (joins to top main cell board)
 
@@ -417,20 +468,20 @@ int main(int argc, char** argv)
 		{
 			float finger_start_y = origin_y + wallgaps[1] + y_step*cury + hole/2.0 - finger_size_y/2.0;
 			float finger_end_y = finger_start_y + finger_size_y;
-			fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length+2.0*thickness+lasertrim, finger_start_y+lasertrim);
-			fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length+thickness+lasertrim, finger_start_y+lasertrim);
-			fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length+thickness+lasertrim, finger_end_y-lasertrim);
-			fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length+2.0*thickness+lasertrim, finger_end_y-lasertrim);
+			fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length+lasertrim, finger_start_y+lasertrim);
+			fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length-thickness+lasertrim, finger_start_y+lasertrim);
+			fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length-thickness+lasertrim, finger_end_y-lasertrim);
+			fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length+lasertrim, finger_end_y-lasertrim);
 		}
 
-		fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length+2.0*thickness+lasertrim, outline[2][Y]+thickness+lasertrim);
+		fprintf(gfile, "G01 X%.2f Y%.2f\n", front_origin_x+cell_length+lasertrim, outline[2][Y]+thickness+lasertrim);
 
 		// Top horizontal (joins to another side board)
 
 		for(int curx = num_side_front_fingers-1; curx >= 0; curx--)
 		{
-			float finger_end_x = front_origin_x + side_front_finger_step/2.0 +
-				side_front_finger_step*curx - side_front_finger_step/4.0;
+			float finger_end_x = front_origin_x + thickness +
+				side_front_finger_step*curx;
 			float finger_start_x = finger_end_x + side_front_finger_step/2.0;
 			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_start_x-lasertrim, outline[2][Y]+thickness+lasertrim);
 			fprintf(gfile, "G01 X%.2f Y%.2f\n", finger_start_x-lasertrim, outline[2][Y]+lasertrim);
@@ -445,7 +496,7 @@ int main(int argc, char** argv)
 		// Cut ventilation holes
 
 		float fhole_width = (hole+cellgap) - front_mid_width;
-		float fhole_hstep = (cell_length - 2.0*front_y_frame_width)/((float)num_front_holes_y);
+		float fhole_hstep = (cell_length - 2.0*thickness - 2.0*front_y_frame_width)/((float)num_front_holes_y);
 		float fhole_height = fhole_hstep-front_mid_width;
 
 		for(int cury = 0; cury < ys[0]; cury++)
@@ -472,9 +523,10 @@ int main(int argc, char** argv)
 			}
 		}
 
-		// OpenSCAM bug workaround:
-		fprintf(gfile, "G00 X%.2f Y%.2f\n", front_origin_x-lasertrim, outline[3][Y]+thickness+lasertrim);
 	}
+
+	// OpenSCAM bug workaround:
+	fprintf(gfile, "G00 X%.2f Y%.2f\n", origin_x, origin_y);
 
 	fclose(gfile);
 
